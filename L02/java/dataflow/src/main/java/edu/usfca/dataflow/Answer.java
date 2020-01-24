2d7c0c172845bb02d25db818907408ec0f25f7d5
haden <haden.lee@molocoads.com>
2020-01-24 15:49:01 -0800

    adding sample code from lecture 2

---
+++ L02/java/dataflow/src/main/java/edu/usfca/dataflow/Answer.java
@@ -0,0 +1,23 @@
+package edu.usfca.dataflow;
+
+public class Answer {
+  static int answer1() {
+    return 8;
+  }
+
+  static int answer2() {
+    return 12;
+  }
+
+  static int answer3() {
+    return 41;
+  }
+
+  static String answer4() {
+    return "fridays";
+  }
+
+  static String answer5() {
+    return "search";
+  }
+}
